55a2b6d233dbefcbf21984d34ca2b4f85ab39d74
zipeks <matzielinski7@gmail.com>
2022-11-11 20:11:31 +0100

    up to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    people.sort((p1,p2) => {
+        if (!p1.yearOfDeath) {
+            const today = new Date();
+            p1.yearOfDeath = today.getFullYear();
+        }
+        if (!p2.yearOfDeath) {
+            const today = new Date();
+            p2.yearOfDeath = today.getFullYear();
+        }
+        if ((p2.yearOfDeath-p2.yearOfBirth) > (p1.yearOfDeath-p1.yearOfBirth)) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+    console.log(people[0]);
+    return people[0];
 };
 
 // Do not edit below this line
